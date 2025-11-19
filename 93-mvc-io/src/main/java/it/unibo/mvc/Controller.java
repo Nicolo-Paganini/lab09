c118efebe95ed53895ec899d3130d170ddc0d15c
Nicolò Paganini <paganininicolo1@gmail.com>
2025-11-19 01:09:43 +0100

    --no-edit

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,29 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
 
+    /**
+     * @param nextString next string to print
+     */
+    void setNextStringToPrint(String nextString);
+
+    /**
+     * @return nextString to print
+     */
+    String getNextStringToPrint();
+
+    /**
+     * @return history of printed strings as a list
+     */
+    List<String> getPrintedStringsHistory();
+
+    /**
+     *
+     */
+    void printCurrentString();
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,21 @@
 package it.unibo.mvc;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +24,63 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private final Controller controller;
 
+    public SimpleGUI(final Controller ctrl){
+        this.controller = ctrl;
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        final JTextField textField = new JTextField();
+        panel.add(textField, BorderLayout.NORTH);
+        final JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        panel.add(textArea, BorderLayout.CENTER);
+         final JPanel southPanel = new JPanel();
+        southPanel.setLayout(new BoxLayout(southPanel, BoxLayout.LINE_AXIS));
+        panel.add(southPanel, BorderLayout.SOUTH);
+        final JButton print = new JButton("PRINT");
+        final JButton showHistory = new JButton("Show_History");
+        southPanel.add(print);
+        southPanel.add(showHistory);
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        print.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e){
+                SimpleGUI.this.controller.setNextStringToPrint(textField.getText());
+                SimpleGUI.this.controller.printCurrentString();
+            }
+        });
+
+        showHistory.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e){
+                final StringBuilder text = new StringBuilder();
+                final List<String> history = SimpleGUI.this.controller.getPrintedStringsHistory();
+                for (final String print: history) {
+                    text.append(print).append('\n');
+                }
+                if (!history.isEmpty()) {
+                    text.deleteCharAt(text.length() - 1);
+                }
+                textArea.setText(text.toString());
+            }
+        });
+
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 5, sh / 5);
+
+        frame.setLocationByPlatform(true);
+    }
+
+    public void display(){
+        frame.setVisible(true);
+    }
+
+    public static void main(final String[] args){
+        new SimpleGUI(new SimpleController()).display();
+    }
 }
